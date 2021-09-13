b3bd029cb4f1fa83da81bd2837a4a230c85a6ea4
Ccrane02 <89466273+Ccrane02@users.noreply.github.com>
2021-09-12 22:15:47 -0500

    Make a scene

---
+++ Unit1/Make_A_Scene/sketch.js
@@ -0,0 +1,52 @@
+function setup() {
+  // Sets the screen to be 720 pixels wide and 400 pixels high
+  createCanvas(720, 400);
+  background(255,126,4);
+  noStroke();
+}
+function draw() {
+  // Sets the screen to be 720 pixels wide and 400 pixels high
+
+
+
+
+
+
+  fill(255);
+  ellipse(18,18 , 72, 72);
+
+  fill(255);
+  ellipse(71,17 , 72, 72);
+  fill(255);
+  ellipse(701,17 , 72, 72);
+   fill(255);
+  ellipse(640,17 , 72, 72);
+   fill(255);
+  ellipse(538,99 , 72, 72);
+   fill(255);
+  ellipse(576,99 , 72, 72);
+   fill(255);
+  ellipse(555,70 , 72, 72);
+   fill(255);
+  ellipse(146,99 , 72, 72);
+   fill(255);
+  ellipse(187,99 , 72, 72);
+   fill(255);
+  ellipse(166,70 , 72, 72);
+fill(158,96,2);
+  fill(100, 200, 80);
+  rect(0, 360, 720, 40);
+  fill("brown");
+  quad(126,225,161,225,167, 360, 100,360);
+    fill(100, 200, 80);
+   ellipse (135,225,72,72);
+    fill(100, 200, 80);
+   ellipse (107,225,72,72);
+    fill(100, 200, 80);
+   ellipse (165,225,72,72);
+    fill(100, 200, 80);
+   ellipse (135,180,72,72);
+  fill(255,78,4);
+  arc(360,360, 300, 300, PI, TWO_PI);
+
+}
