67ef1eeb16a2cc05617af9a8a5c3f35799aea3dd
Ccrane02 <89466273+Ccrane02@users.noreply.github.com>
2021-10-10 17:40:13 -0500

    audioin

--- Unit2/audioIn/sketch.js
+++ Unit2/audioIn/sketch.js
@@ -29,7 +29,7 @@ function draw() {
   textSize(18);
   text("Click the screen first to give\npermission for mic input.\nMy volume is " + vol, 10, 60);
 
-  rect(vol * 100, 100, 50, 50);
+  ellipse(vol * 100, 100, 50, 50);
 
 //   rect(vol * 300, 150, 50, 50);
 
