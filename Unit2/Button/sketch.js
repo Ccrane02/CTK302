cb3ce54ef2e4d7d8b3e56abf41d1f0c34c4126e6
Ccrane02 <89466273+Ccrane02@users.noreply.github.com>
2021-10-10 16:59:51 -0500

    Button

--- Unit2/Button/sketch.js
+++ Unit2/Button/sketch.js
@@ -1,7 +1,43 @@
+let state = 0;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
+  rectMode(CENTER) ;
 }
 
 function draw() {
+
+  background(100);
+  fill(255) ;
+  text(mouseX + ", " + mouseY, 50, 50) ;
+
+  switch (state) {
+
+    case 0:
+      text("0", 300, 100);
+      break;
+
+    case 1:
+      text("1", 300, 100);
+      break;
+
+    case 2:
+      text("2", 300, 100);
+      break;
+
+  }
+
+  fill('red');
+  rect(width/2, height-75, 100, 50);
+
+}
+
+function mouseReleased() {
+  if ((mouseX > width/2-50) && (mouseX < width/2+50) && (mouseY > height-75) && (mouseY < height-25)) {
+
+  state++;
+
+    if (state > 2) state = 0;
+  }
 
 }
