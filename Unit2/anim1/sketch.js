5ee4e75c0498e0349bb539f1817b1c013e1c0a83
Ccrane02 <89466273+Ccrane02@users.noreply.github.com>
2021-10-10 17:14:25 -0500

    anim1

--- Unit2/anim1/sketch.js
+++ Unit2/anim1/sketch.js
@@ -7,6 +7,9 @@ function draw() {
 background('grey');
 ellipse(x,100,100,100);
 ellipse(x,200,100,100);
+ellipse(x,300,100,100);
+ellipse(x,400,100,100);
+ellipse(x,500,100,100);
 x=x+5;
 if(x >= 500) {
   x=0;
