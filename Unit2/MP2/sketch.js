6e3bae18601bbf3c9c5e139b344bc94c250b7172
Ccrane02 <89466273+Ccrane02@users.noreply.github.com>
2021-10-10 16:39:21 -0500

    MP2

--- Unit2/MP2/sketch.js
+++ Unit2/MP2/sketch.js
@@ -1,35 +1,55 @@
-let turtle;
+let turtle,don,mich,leo,raph,pizza;
 let state = 0;
 let timer = 0;
 var f1;
+let x=0;
 function setup() {
-  createCanvas(700, 700);
+  createCanvas(900, 700);
   turtle = loadImage("assets/turtle.jpg");
+  don = loadImage("assets/don.gif");
+  mich = loadImage("assets/mich.gif");
+  leo = loadImage("assets/leo.gif");
+  raph = loadImage("assets/raph.gif");
+  pizza = loadImage('assets/pizza.jpg');
   f1 = loadFont("assets/font.ttf");
   imageMode(CENTER);
 }
 
 function draw() {
-  background('white');
-  image(turtle, width / 2, height / 2, 700, 700);
   switch(state) {
 
     case 0:
     background('white');
-    image(turtle, width / 2, height / 2, 700, 700);
+    image(turtle, width / 2, height / 2, 900, 700);
     textFont(f1);
     textSize(24);
     text("Press to Start",250 ,250 );
+
       break ;
        case 1:
       background('white');
-      image(turtle, width / 2, height / 2, 700, 700);
-    }
+      image(turtle, width / 2, height / 2, 900, 700);
+  translate(x,0);
+      image(don,0,550,300,300);
+      image(leo,900,550,300,300);
+      image(raph,300,550,300,300);
+      image(mich,600,550,300,300);
+x=x+2;
+      if(x>900){
+        x=-300
+      }
+      break;
+      case 2:
+      background('white');
+      image(pizza,width/2,height/2,900,700)
+      break;
+  }
+
       timer++ ;
-   if (timer > 60*60) {
+   if (timer > 7*60) {
      timer=0;
      state++;
-     if (state>1) {
+     if (state>3) {
        state=0;
    }
 
@@ -37,9 +57,10 @@ function draw() {
 
 
 }
+
 function mouseReleased(){
   state++ ;
-  if(state>1){
+  if(state>3){
     state=0;
   }
 
